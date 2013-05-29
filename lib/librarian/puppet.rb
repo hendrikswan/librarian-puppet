d9e7b8fa0da205075805cb4ebef3a2c677be51e9
Hendrik Swanepoel <hendrik@22seven.com>
2013-05-29 21:21:13 +0200

    monkey patch in order to get puppet loading on windows

--- lib/librarian/puppet.rb
+++ lib/librarian/puppet.rb
@@ -1,6 +1,30 @@
 require 'librarian'
 require 'fileutils'
 
+class File
+  class << self
+    alias_method :original_join, :join
+  end
+
+  def self.join(*args)
+    new_args = args.collect { |questionableEncoding|
+      join_encoding_fix(questionableEncoding)
+    }
+    self.send(:original_join, new_args)
+  end
+
+  def self.join_encoding_fix(value)
+    if (value.instance_of?(String))
+      value = value.encode("UTF-8")
+    elsif (value.instance_of?(Array))
+      value = value.collect { |subValue|
+        join_encoding_fix(subValue)
+      }
+    end
+    value
+  end
+end
+
 begin
   require 'puppet'
 rescue LoadError
